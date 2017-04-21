fcb184101934af34e6bcd12d021dc85e869d5632
Shaun Hogan <shaun.graphic1@gmail.com>
2017-04-21 11:49:35 +0200

    Update doc-embed-script.js

--- doc-embed-script.js
+++ doc-embed-script.js
@@ -58,50 +58,42 @@
                         onclick: function( e ) {
 
                             jQuery(function($){
-                                // Set all variables to be used in scope
+
                                 var frame;
-                                //it has to match the "textareaID" above, because it is the input field that we are
-                                //going to insert the data in HTML format.
+
                                 var imgContainer = $( '.mce-doc-link' );
 
-                                // ADD IMAGE LINK
                                 event.preventDefault();
 
-                                // If the media frame already exists, reopen it.
                                 if ( frame ) {
                                     frame.open();
                                     return;
                                 }
 
-                                // Create a new media frame
                                 frame = wp.media({
                                     title: 'Select or Upload Media',
                                     button: {
                                       text: 'Use this media'
                                     },
-                                    multiple: false  // Set to true to allow multiple files to be selected
+                                    multiple: false  
                                 });
 
-                                // When an image is selected in the media frame...
                                 frame.on( 'select', function() {
 
-                                    // Get media attachment details from the frame state
                                     var attachment = frame.state().get('selection').first().toJSON();
 
-                                    // Send the attachment URL to our custom image input field.
                                     var imageContent = attachment.url;
                                     imgContainer.val( imageContent );
 
                                 });
 
-                                // Finally, open the modal on click
                                 frame.open();
                         });
                         return false;
                         }
                     }],
                     onsubmit: function( e ) {
-                        // wrap it with a div and give it a class name
+
                         editor.insertContent( '[googleembed link="' + e.data.link + '" title="' + e.data.title + '" btn="' + e.data.button + '" width="' + e.data.width + '" height="' + e.data.height + '" btn-class="' + e.data.class + '"]');
                     }
                 });
